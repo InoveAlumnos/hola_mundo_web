fd4af57970b33bbec4e513710732f6e71fa2137c
Lettuce <85661679+CLettuce@users.noreply.github.com>
2023-04-11 13:46:20 -0600

    Update console.js

--- console.js
+++ console.js
@@ -1,2 +1,9 @@
 console.log("Hola")
 console.log("Hola") //2nd
+console.log("Hola") //2nd
+
+console.log("Hola") //2nd
+
+console.log("Hola") //2nd
+console.log("Hola") //2nd
+console.log("Hola") //2nd
